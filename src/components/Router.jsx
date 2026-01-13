afda496213b19dfecb3158ffd65244b1888dbff5
ChaberJo <cjf.humbert@gmail.com>
2026-01-13 22:32:45 +0100

    Add error page path

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,8 +1,10 @@
 import App from './App.jsx'
 import About from '../pages/About.jsx'
+import Error from '../pages/Error.jsx'
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
         {path:'/', element: <App />},
         {path:'/about', element: <About/>},
+        {path:'*', element: <Error/>},
     ]);
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import './styles/index.css'
-import App from './components/App.jsx'
-import About from './pages/About.jsx'
 import { router } from './components/Router.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
