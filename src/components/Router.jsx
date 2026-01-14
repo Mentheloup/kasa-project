dceb3b7023964400a85f00f632a65af4e19e0c00
ChaberJo <cjf.humbert@gmail.com>
2026-01-14 09:59:33 +0100

    Add logement to router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,10 +1,12 @@
 import App from './App.jsx'
 import About from '../pages/About.jsx'
 import Error from '../pages/Error.jsx'
+import Logement from '../pages/Logement.jsx'
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
         {path:'/', element: <App />},
         {path:'/about', element: <About/>},
         {path:'*', element: <Error/>},
+        {path:'/logement', element: <Logement/>},
     ]);
