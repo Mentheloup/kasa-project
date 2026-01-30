86b2cf3f62ed6d9eee8798fb73ecec91015b0416
ChaberJo <cjf.humbert@gmail.com>
2026-01-30 15:57:16 +0100

    Continue slider

--- src/components/Slider/slider.jsx
+++ src/components/Slider/slider.jsx
@@ -1,8 +1,18 @@
 import './slider.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import valeurs from '../../datas/valeurs.json'
+
 function Slider () {
     return (
-        <div className="sliderBox">
-            <h1>Fiabilité</h1>
+        <div className='slider'>
+            {valeurs.map ((chaqueValeur) =>
+                <div key={chaqueValeur.id} className="sliderBox">
+                    <h1>{chaqueValeur.name}</h1>
+                    <FontAwesomeIcon icon={faAngleUp} size='xl' />
+                </div>
+            )}
+            
         </div>
     )
 }
