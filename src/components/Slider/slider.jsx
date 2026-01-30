96a015ca8f90442d5239cdf3260018171a07edff
ChaberJo <cjf.humbert@gmail.com>
2026-01-30 23:48:21 +0100

    Working on slider

--- src/components/Slider/slider.jsx
+++ src/components/Slider/slider.jsx
@@ -2,17 +2,21 @@ import './slider.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
 import valeurs from '../../datas/valeurs.json'
+import { useState } from 'react'
 
 function Slider () {
+
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <div className='slider'>
             {valeurs.map ((chaqueValeur) =>
                 <div key={chaqueValeur.id}>
                     <div className="sliderBox">
                         <h1>{chaqueValeur.name}</h1>
-                        <FontAwesomeIcon icon={faAngleUp} size='xl' />
+                        <FontAwesomeIcon icon={faAngleUp} className='arrow' size='xl' onClick={() => setIsOpen(true)} />
                     </div>
-                    <p>{chaqueValeur.text}</p>
+                    <p className='valueTxt'>{chaqueValeur.text}</p>
                 </div>
             )}
             
